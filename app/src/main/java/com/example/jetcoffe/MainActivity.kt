0206274c91f2d4f7dc125535f45811337e14e40a
Gilang <gilangnurrizki173@gmail.com>
2025-02-01 16:28:07 +0700

    add menu

--- app/src/main/java/com/example/jetcoffe/MainActivity.kt
+++ app/src/main/java/com/example/jetcoffe/MainActivity.kt
@@ -13,6 +13,8 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -20,8 +22,12 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetcoffe.model.Menu
+import com.example.jetcoffe.model.dummyBestSellerMenu
 import com.example.jetcoffe.model.dummyCategory
+import com.example.jetcoffe.model.dummyMenu
 import com.example.jetcoffe.ui.components.CategoryItem
+import com.example.jetcoffe.ui.components.MenuItem
 import com.example.jetcoffe.ui.components.Search
 import com.example.jetcoffe.ui.components.SectionText
 import com.example.jetcoffe.ui.theme.JetCoffeTheme
@@ -40,10 +46,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp(modifier: Modifier = Modifier) {
-    Column {
+    Column (
+        modifier = Modifier.verticalScroll(rememberScrollState())
+    ) {
         Banner()
         SectionText(stringResource(R.string.section_category))
         CategoryRow()
+        SectionText(stringResource(R.string.section_favorite_menu))
+        MenuRow(dummyMenu)
+        SectionText(stringResource(R.string.section_best_seller_menu))
+        MenuRow(dummyBestSellerMenu)
     }
 }
 
@@ -73,11 +85,19 @@ fun CategoryRow(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-private fun CategoryRowPreview() {
-    JetCoffeTheme {
-        CategoryRow()
+fun MenuRow(
+    listMenu: List<Menu>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow (
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(listMenu, key = { it.title }) { menu ->
+            MenuItem(menu)
+        }
     }
 }
 
