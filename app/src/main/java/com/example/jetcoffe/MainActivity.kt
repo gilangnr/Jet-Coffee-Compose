e86b27f56ba6472c7dc80561845d1f9f0ca58480
Gilang <gilangnurrizki173@gmail.com>
2025-02-01 20:46:11 +0700

    add bottom navigation

--- app/src/main/java/com/example/jetcoffe/MainActivity.kt
+++ app/src/main/java/com/example/jetcoffe/MainActivity.kt
@@ -15,6 +15,16 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -22,14 +32,15 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.jetcoffe.model.BottomBarItem
 import com.example.jetcoffe.model.Menu
 import com.example.jetcoffe.model.dummyBestSellerMenu
 import com.example.jetcoffe.model.dummyCategory
 import com.example.jetcoffe.model.dummyMenu
 import com.example.jetcoffe.ui.components.CategoryItem
+import com.example.jetcoffe.ui.components.HomeSection
 import com.example.jetcoffe.ui.components.MenuItem
 import com.example.jetcoffe.ui.components.Search
-import com.example.jetcoffe.ui.components.SectionText
 import com.example.jetcoffe.ui.theme.JetCoffeTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,16 +57,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp(modifier: Modifier = Modifier) {
-    Column (
-        modifier = Modifier.verticalScroll(rememberScrollState())
-    ) {
-        Banner()
-        SectionText(stringResource(R.string.section_category))
-        CategoryRow()
-        SectionText(stringResource(R.string.section_favorite_menu))
-        MenuRow(dummyMenu)
-        SectionText(stringResource(R.string.section_best_seller_menu))
-        MenuRow(dummyBestSellerMenu)
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_favorite_menu),
+                content = { MenuRow(dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) }
+            )
+        }
     }
 }
 
@@ -97,6 +120,46 @@ fun MenuRow(
     ) {
         items(listMenu, key = { it.title }) { menu ->
             MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                onClick = {}
+            )
         }
     }
 }
